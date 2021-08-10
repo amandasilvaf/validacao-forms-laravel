1d97ba576b6cb3198ac4238b6a2d678ee43a7219
Amanda Ferreira <amandasilvaf1995@gmail.com>
2021-08-10 16:43:11 -0300

    Invoca o Bootstrap e outros arquivos em home.blade.php
    Para registrar o helper url(mix())

--- resources/views/site/home.blade.php
+++ resources/views/site/home.blade.php
@@ -5,12 +5,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Laravel-mix</title>
+
+    <link rel="stylesheet" href="{{ url(mix('bootstrap/bootstrap.css'))}}">
     <link rel="stylesheet" href="{{ url(mix('site/css/style.css')) }}">
+
 </head>
 <body>
     <h1>Hello, Laravel-mix!!!</h1>
+    
 
-
-    <script src="{{ url(mix('site/js/script.js')) }}"></script>
+    <script src="{{ url(mix('jquery/jquery.js')) }}"></script>
+    <script src="{{ url(mix('bootstrap/bootstrap.js')) }}"></script>
+    <script src="{{ url(mix('site/js/script.js')) }}"></script> {{--Meu js!--}}
 </body>
 </html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -12,5 +12,8 @@ const mix = require('laravel-mix');
  */
 
  mix.styles(['resources/views/site/css/reset.css', 'resources/views/site/css/style.css'], 'public/site/css/style.css')
-    .scripts(['resources/views/site/js/script.js'],'public/site/js/script.js')
- .version();
+    .scripts('resources/views/site/js/script.js','public/site/js/script.js')
+    .scripts('node_modules/jquery/dist/jquery.js','public/jquery/jquery.js')
+    .scripts('node_modules/bootstrap/dist/js/bootstrap.bundle.js', 'public/bootstrap/bootstrap.js')
+    .sass('node_modules/bootstrap/scss/bootstrap.scss','public/bootstrap/bootstrap.css')
+    .version();

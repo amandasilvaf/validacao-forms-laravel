0d3e4eb4d48aec743dbc73be6cb1f963da347f93
Amanda Ferreira <amandasilvaf1995@gmail.com>
2021-08-10 16:02:13 -0300

    Configura webpack.mix.js

---
+++ resources/views/site/home.blade.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel-mix</title>
+    <link rel="stylesheet" href="{{ url(mix('site/css/style.css')) }}">
+</head>
+<body>
+    <h1>Hello, Laravel-mix!!!</h1>
+
+
+    <script src="{{ url(mix('site/js/script.js')) }}"></script>
+</body>
+</html>
--- webpack.mix.js
+++ webpack.mix.js
@@ -6,12 +6,11 @@ const mix = require('laravel-mix');
  |--------------------------------------------------------------------------
  |
  | Mix provides a clean, fluent API for defining some Webpack build steps
- | for your Laravel applications. By default, we are compiling the CSS
+ | for your Laravel application. By default, we are compiling the Sass
  | file for the application as well as bundling up all the JS files.
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+ mix.styles(['resources/views/site/css/reset.css', 'resources/views/site/css/style.css'], 'public/site/css/style.css')
+    .scripts(['resources/views/site/js/script.js'],'public/site/js/script.js')
+ .version();
